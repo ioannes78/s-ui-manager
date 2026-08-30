4f3a4474145e86de2fdd4fef2906799beeaaebe5
ioannes78 <70731599+ioannes78@users.noreply.github.com>
2026-08-30 01:11:35 -0700

    fix: register node detail tab components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import {
-  ElAlert, ElButton, ElCard, ElCheckbox, ElDatePicker, ElDialog, ElDrawer,
+  ElAlert, ElButton, ElCard, ElCheckbox, ElDatePicker, ElDialog, ElDrawer, ElEmpty,
   ElForm, ElFormItem, ElInput, ElInputNumber, ElLoading, ElOption, ElProgress,
-  ElRadioButton, ElRadioGroup, ElSelect, ElSwitch, ElTable, ElTableColumn, ElTag,
+  ElRadioButton, ElRadioGroup, ElSelect, ElSwitch, ElTable, ElTableColumn, ElTabPane,
+  ElTabs, ElTag,
 } from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style.css'
@@ -11,9 +12,10 @@ import { router } from './router'
 
 const application = createApp(App)
 const elementComponents = [
-  ElAlert, ElButton, ElCard, ElCheckbox, ElDatePicker, ElDialog, ElDrawer,
+  ElAlert, ElButton, ElCard, ElCheckbox, ElDatePicker, ElDialog, ElDrawer, ElEmpty,
   ElForm, ElFormItem, ElInput, ElInputNumber, ElLoading, ElOption, ElProgress,
-  ElRadioButton, ElRadioGroup, ElSelect, ElSwitch, ElTable, ElTableColumn, ElTag,
+  ElRadioButton, ElRadioGroup, ElSelect, ElSwitch, ElTable, ElTableColumn, ElTabPane,
+  ElTabs, ElTag,
 ]
 elementComponents.forEach((component) => application.use(component))
 application.use(router).mount('#app')
